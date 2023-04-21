a8efe4ec9bf649c94651f8f0460dbdfe55d0310e
m.lozanov23 <m.lozanov23@acsbg.org>
2023-04-21 11:46:28 +0300

    Add project files.

---
+++ SignUp.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FINAL_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignUp_Button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
